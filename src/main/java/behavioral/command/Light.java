6acf2d844c94b29721ec1a0ce3400d516778c8e0
Mohammed Shoukath Ali <shoukathmd@gmail.com>
2023-09-20 11:05:31 +0530

    Command Pattern

---
+++ src/main/java/behavioral/command/Light.java
@@ -0,0 +1,12 @@
+package behavioral.command;
+
+public class Light {
+
+    public void turnOn() {
+        System.out.println("Light is turned on");
+    }
+
+    public void turnOff(){
+        System.out.println("Light is turned off");
+    }
+}
